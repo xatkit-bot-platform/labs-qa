46f0ddd821ee86806ade381a2cce12b71b202d39
Marcos Gomez <gomezmarcos99@gmail.com>
2021-05-31 19:53:56 +0200

    update QuestionAnswering module

--- app.py
+++ app.py
@@ -1,55 +1,185 @@
 from flask import Flask, request
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, BertConfig, LongformerConfig
+
+"""
+# Haystack
+
+from haystack.pipeline import ExtractiveQAPipeline
+from haystack.document_store.memory import InMemoryDocumentStore
+
+from haystack.preprocessor.utils import convert_files_to_dicts
+from haystack.reader.transformers import TransformersReader
+from haystack.retriever.sparse import TfidfRetriever
+
+document_store = InMemoryDocumentStore()
+doc_dir = "src/main/resources"
+dicts = convert_files_to_dicts(dir_path=doc_dir, split_paragraphs=True)
+document_store.write_documents(dicts)
+retriever = TfidfRetriever(document_store=document_store)
+reader = TransformersReader(model_name_or_path=name, tokenizer=name, use_gpu=-1)
+pipe = ExtractiveQAPipeline(reader, retriever)
+"""
+
+
+
 import torch
 import logging
 
 logging.basicConfig(level=logging.INFO)
 
-name1 = "bert-large-uncased-whole-word-masking-finetuned-squad"
-name2 = "deepset/roberta-base-squad2"
-name = name1
+MAX_TOKENS = 512
+MAX_TOKENS_XL = 4096
 
-logging.info("Loading " + name + " tokenizer")
-tokenizer = AutoTokenizer.from_pretrained(name)
-logging.info(name + " tokenizer loaded")
+modelNames = []
+models = []
+tokenizers = []
 
-logging.info("Loading " + name + " model")
-model = AutoModelForQuestionAnswering.from_pretrained(name)
-logging.info(name + " model loaded")
-
-
-
+# configuration = BertConfig(max_position_embeddings=MAX_TOKENS)
 app = Flask(__name__)
 
-@app.route('/qa', methods=['POST'])
-def processQA():    
+def splitParagraph(tokenizer, question, paragraph):
+    inputs = tokenizer(question, paragraph, add_special_tokens=True, return_tensors="pt")
+    input_ids = inputs["input_ids"].tolist()[0]
+    if (len(input_ids) > MAX_TOKENS):
+        p1 = paragraph[0:int(len(paragraph)/2)]
+        p2 = paragraph[int(len(paragraph)/2):]
+        return splitParagraph(tokenizer, question, p1) + splitParagraph(tokenizer, question, p2)
+    else:
+        return [paragraph]
+    
+    
+@app.route('/split-qa', methods=['POST'])
+def transformerSplitQA():    
+    body = request.get_json()
+    question = body["question"]
+    corpus_raw = body["corpus"]
+    corpus = []
+    response = {}
+    n_tokens = ""
+    for i, name in enumerate(names):
+        tokenizer = tokenizers[i]
+        model = models[i]
+        
+        inputs = tokenizer(question, corpus_raw, add_special_tokens=True, return_tensors="pt")
+        input_ids = inputs["input_ids"].tolist()[0]
+        n_tokens += (str(len(input_ids)) + " | ")
+        
+        if (len(input_ids) > MAX_TOKENS):
+            corpus = list(filter(lambda x: x != "", corpus_raw.split('\n')))
+            for i, paragraph in enumerate(corpus):
+                paragraph_inputs = tokenizer(question, paragraph, add_special_tokens=True, return_tensors="pt")
+                paragraph_input_ids = paragraph_inputs["input_ids"].tolist()[0]
+                if (len(paragraph_input_ids) > MAX_TOKENS):
+                    corpus[i:i+1] = splitParagraph(tokenizer, question, paragraph)
+        else:
+            corpus = [corpus_raw] 
+            
+        best_answer = ""
+        context = ""
+        best_score = 0 # Are best scores always > 0 ???
+        paragraph_id = -1
+        for i, paragraph in enumerate(corpus):
+            #print(paragraph)
+            inputs = tokenizer(question, paragraph, add_special_tokens=True, return_tensors="pt")
+            input_ids = inputs["input_ids"].tolist()[0]
+            corpus_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+            outputs = model(**inputs)
+            answer_start_scores = outputs.start_logits
+            answer_end_scores = outputs.end_logits
+            score_start = torch.max(answer_start_scores)
+            score_end = torch.max(answer_end_scores)
+            score = score_start + score_end
+            answer_start = torch.argmax(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
+            answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+            answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+            # PROBLEMA: SCORES DE NO RESPUESTA ALTOS EN OTROS PARAGRAFOS???
+            if (score > best_score and answer != "[CLS]"):
+                best_score = score
+                best_answer = answer
+                context = paragraph
+                paragraph_id = i
+                
+                
+        response[name] = {
+        	"answer": best_answer,
+            "context": context,
+            "paragraph_id": paragraph_id
+        }
+    print(n_tokens)
+
+    return response, 200
+
+@app.route('/set-models', methods=['POST'])
+def setModels():
+    body = request.get_json()
+    names = body["modelNames"]
+    
+    for name in names:
+        modelNames.append(name)
+        logging.info("Loading " + name + " tokenizer")
+        tokenizer = AutoTokenizer.from_pretrained(name)
+        logging.info(name + " tokenizer loaded")
+        logging.info("Loading " + name + " model")
+        model = AutoModelForQuestionAnswering.from_pretrained(name)
+        logging.info(name + " model loaded")
+        print("------------------------------------")
+        models.append(model)
+        tokenizers.append(tokenizer)
+    response = {
+        "status": "done"
+        }
+    return response, 200
+    
+@app.route('/transformer-qa', methods=['POST'])
+def transformerQA():    
     body = request.get_json()
     question = body["question"]
     corpus = body["corpus"]
-    inputs = tokenizer(question, corpus, add_special_tokens=True, return_tensors="pt")
-    print(inputs)
-    print("========================================")
-    input_ids = inputs["input_ids"].tolist()[0]
-    corpus_tokens = tokenizer.convert_ids_to_tokens(input_ids)
-    print(corpus_tokens)
-    outputs = model(**inputs)
-    answer_start_scores = outputs.start_logits
-    answer_end_scores = outputs.end_logits
-    answer_start = torch.argmax(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
-    answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
-    answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
-    print(f"Question: {question}")
-    print(f"Answer: {answer}")
+    response = {}
+    for i, modelName in enumerate(modelNames):
+        tokenizer = tokenizers[i]
+        model = models[i]
+        encoding = tokenizer(question, corpus, add_special_tokens=True, return_tensors="pt")
+        input_ids = encoding["input_ids"].tolist()[0]
+        corpus_tokens = tokenizer.convert_ids_to_tokens(input_ids)
+        outputs = model(**encoding)
+        answer_start_scores = outputs.start_logits
+        answer_end_scores = outputs.end_logits
+        answer_start = torch.argmax(answer_start_scores)  # Get the most likely beginning of answer with the argmax of the score
+        answer_end = torch.argmax(answer_end_scores) + 1  # Get the most likely end of answer with the argmax of the score
+        answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
+
+        response[modelName] = {
+            "question": question,
+        	"answer": answer,
+            "beginPosition": answer_start.item(),
+            "endPosition": answer_end.item()
+        }
+        
+    return response, 200
+
     
-    response = {
-    	"answer": answer,
-    	"beginPosition": str(answer_start.item()),
-    	"endPosition": str(answer_end.item())
-    }
+@app.route('/haystack-qa', methods=['POST'])
+def haystackQA():
+    body = request.get_json()
+    question = body["question"]
+    """
+    Prediction attributes
+        - answer
+        - context
+        - document_id
+        - meta
+            - name
+        - offset_start
+        - offset_end
+        - probability
+        - score
+    """
+    prediction = pipe.run(query=question, top_k_retriever=10, top_k_reader=5)
+    answer = prediction["answers"][0]
+    response = prediction
     
     return response, 200
-    
-    
     
     
     
--- src/main/java/QuestionAnswering.java
+++ src/main/java/QuestionAnswering.java
@@ -3,79 +3,147 @@ import lombok.Getter;
 import lombok.Setter;
 import org.json.JSONObject;
 import fr.inria.atlanmod.commons.log.Log;
-import org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-import static java.util.Objects.isNull;
 
+/**
+ * The main class to run a QuestionAnswering pipeline with Huggingface endpoints (language models).
+ * <p>
+ * To run this, it is needed to deploy a server that runs the language models to do HTTP requests to the server,
+ * asking for answers to a given question and corpus.
+ */
 public class QuestionAnswering {
 
+    /**
+     * The address to the server running the language models.
+     */
     @Getter
     @Setter
-    private String question;
+    private static String address = "http://127.0.0.1:5000/";
 
-    @Getter
-    private String answer;
 
-    @Getter
-    @Setter
-    private String corpus;
-
-    @Getter
-    private int beginPosition;
-
-    @Getter
-    private int endPosition;
-
-    @Getter
-    @Setter
-    private String address;
-
-    private final String CORPUS_FILE = "corpus.txt";
-
-    public void requestAnswer() {
+    public void testQA(String endpoint) {
+        /*
         JSONObject request = new JSONObject();
         request.put("question", question);
         request.put("corpus", corpus);
         JSONObject response = new JSONObject();
         try {
-            response = Unirest.post(address + "qa")
+            response = Unirest.post(address + endpoint)
                     .header("Content-Type", "application/json")
                     .body(request)
                     .asJson().getBody().getObject();
-            answer = response.getString("answer");
-            beginPosition = response.getInt("beginPosition");
-            endPosition = response.getInt("endPosition");
+            this.setAnswer(response.getJSONObject("bert-large-uncased-whole-word-masking-finetuned-squad").getString("answer"));
+            //System.out.println("| " + question + " | " +
+                    //response.getJSONObject("bert-large-uncased-whole-word-masking-finetuned-squad").getString("answer") + " | ");
+                    //"["+response.getJSONObject("bert-large-uncased-whole-word-masking-finetuned-squad").getInt("paragraph_id")+"] "+
+                    //response.getJSONObject("bert-large-uncased-whole-word-masking-finetuned-squad").getString("answer") + " | ");
+        } catch (Exception e) {
+            Log.error(e, "An error occurred while computing the answer, see the attached exception");
+        }
+
+         */
+    }
+
+    /**
+     * Process a QuestionAnswering request.
+     *
+     * @param corpus     the corpus
+     * @param question   the question
+     * @param endpoint   the endpoint where to do the request in {@link #address}
+     * @param modelNames the name of the models that will compute an answer for the given question (i.e. one answer
+     *                   per model)
+     * @return the {@link List} containing all generated answers to the given question by the given language models.
+     */
+    public static List<AnswerObject> processQA(String corpus, String question, String endpoint,
+                                               List<String> modelNames) {
+        JSONObject request = new JSONObject();
+        request.put("question", question);
+        request.put("corpus", corpus);
+        request.put("modelNames", modelNames);
+        JSONObject response = new JSONObject();
+        try {
+            response = Unirest.post(address + endpoint)
+                    .header("Content-Type", "application/json")
+                    .body(request)
+                    .asJson().getBody().getObject();
+            List<AnswerObject> answerObjects = new ArrayList<>();
+            for (String modelName : modelNames) {
+                JSONObject modelResponse = response.getJSONObject(modelName);
+                // TODO: check if null????
+                String answer = modelResponse.getString("answer");
+                int beginPosition = modelResponse.getInt("beginPosition");
+                int endPosition = modelResponse.getInt("endPosition");
+                AnswerObject answerObject = new AnswerObject(answer, modelName, beginPosition, endPosition);
+                answerObjects.add(answerObject);
+            }
+            return answerObjects;
+        } catch (Exception e) {
+            Log.error(e, "An error occurred while computing the answer, see the attached exception");
+        }
+        return null;
+    }
+
+    /**
+     * Sets the given language models in the remote server so they are ready to use.
+     *
+     * @param modelNames the model names
+     * @param endpoint   the endpoint where to do the request in {@link #address}
+     */
+    public static void setModelNames(List<String> modelNames, String endpoint) {
+        JSONObject request = new JSONObject();
+        request.put("modelNames", modelNames);
+        try {
+            JSONObject response = new JSONObject();
+            response = Unirest.post(address + endpoint)
+                    .header("Content-Type", "application/json")
+                    .body(request)
+                    .asJson().getBody().getObject();
+            // TODO: check status ok
         } catch (Exception e) {
             Log.error(e, "An error occurred while computing the answer, see the attached exception");
         }
     }
 
-    public QuestionAnswering() {
-        try (InputStream inputStream =
-                     this.getClass().getResourceAsStream(CORPUS_FILE)) {
-            if (isNull(inputStream)) {
-                Log.error("Cannot find the file {0}, this processor won't get any corpus", CORPUS_FILE);
-            } else {
-                setCorpus(IOUtils.toString(inputStream, StandardCharsets.UTF_8));
-            }
-        } catch (IOException e) {
-            Log.error("An error occurred when processing the corpus file {0}, this processor may produce "
-                    + "unexpected behavior. Check the logs for more information.", CORPUS_FILE);
-        }
-        Log.info("Loaded corpus from {0}", CORPUS_FILE);
-    }
-
+    /**
+     * The entry point of application.
+     * <p>
+     * It allows to set the desired language models that will perform the QuestionAnswering, and then to write
+     * questions that will be answered using the given corpus file as the place where the answers should be found.
+     * <p>
+     * If the input is "print" (instead of a question), then all computed Question-Answer pairs are printed
+     * @param args the input arguments.
+     */
     public static void main(String[] args) {
-        QuestionAnswering qa = new QuestionAnswering();
-        qa.setQuestion("<Question goes here>");
-        qa.setAddress("http://127.0.0.1:5000/");
-        qa.requestAnswer();
-        System.out.println("Question: " + qa.getQuestion());
-        System.out.println("Answer: " + qa.getAnswer());
+        // The corpus file must be in the resources folder (/src/main/resources/)
+        QAObject qaObject = new QAObject("pablo_picasso.txt");
+        List<String> modelNames = new ArrayList<String>();
+        modelNames.add("ktrapeznikov/albert-xlarge-v2-squad-v2");
+        modelNames.add("twmkn9/albert-base-v2-squad2");
+        //modelNames.add("mrm8488/bert-tiny-5-finetuned-squadv2");
+        //modelNames.add("bert-large-uncased-whole-word-masking-finetuned-squad");
+        //modelNames.add("distilbert-base-cased-distilled-squad");
+        //modelNames.add("valhalla/longformer-base-4096-finetuned-squadv1");
+        //modelNames.add("google/bigbird-base-trivia-itc");
+
+        //setModelNames(modelNames, "set-models");
+
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            System.out.print("Question: ");
+            String question = in.nextLine();
+            if (question.equals("print")) {
+                qaObject.printQAPairs();
+            }
+            else {
+                List<AnswerObject> answerObjects = processQA(qaObject.getCorpus(), question, "transformer-qa", modelNames);
+                qaObject.addQAPair(question, answerObjects);
+                System.out.println("Answers have been stored. Enter \"print\" to see all question-answers");
+            }
+        }
     }
 
 }
